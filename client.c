e9d1795eb55212dd1dcc3f0108526955bc034f0e
Patricio López <lopezjuripatricio@gmail.com>
2016-11-24 23:41:26 -0300

    Make this thing work

--- client.c
+++ client.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define SERVER "127.0.0.1"
-#define BUFLEN 512  // Max length of buffer
-#define PORT 1029   // The port on which to send data
+#define BUFLEN 1024  // Max length of buffer
+#define PORT 1029    // The port on which to send data
+
+struct Registry {
+  char name[256];
+  char value[256];
+  char type[256];
+  char ttl[256];
+};
+
+char *trim(char *str) {
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0) return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
+}
+
+int valid_ip(char *ip) {
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
+    return result != 0;
+}
 
 int main(int argc, char const *argv[]) {
   struct sockaddr_in si_other;
   socklen_t slen = sizeof(si_other);
   char buf[BUFLEN];
-  char message[BUFLEN];
+  char buffer[BUFLEN];
 
   int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
   if (sock < 0) {
@@ -30,9 +63,18 @@ int main(int argc, char const *argv[]) {
     exit(1);
   }
 
+  // int16_t identifier = 0;
+
   while(1) {
-    printf("Enter message: ");
-    fgets(message, BUFLEN, stdin);
+    printf("Enter query: ");
+    fgets(buffer, BUFLEN, stdin);
+
+    char *message = trim(buffer);
+    // int reverse_query = valid_ip(message);
+    // int16_t id = identifier++;
+    // int16_t flags = 0;
+    // flags &= ~0x01; // 0 = query
+    // flags &= (0b0100 << 4);
 
     // Send the message
     if (sendto(sock, message, strlen(message), 0, (struct sockaddr *) &si_other, slen)==-1) {
@@ -47,9 +89,22 @@ int main(int argc, char const *argv[]) {
       perror("recvfrom()");
     }
 
-    // Show response
-    puts(buf);
+    // Parse response
+    struct Registry *registry = malloc(sizeof(struct Registry));
+    sscanf(buf, "%s %s %s %s", registry->name, registry->value, registry->type, registry->ttl);
+
+    // Show output or do action
+    if (strcmp(registry->type, "A") == 0) {
+      printf("%s has address %s\n", registry->name, registry->value);
+    } else if (strcmp(registry->type, "CNAME") == 0) {
+      printf("%s is an alias for %s\n", registry->name, registry->value);
+      // TODO: requery
+    } else {
+      printf("Host %s not found\n", registry->name);
+    }
   }
+
+  // Shutdown socket
   close(sock);
 
   return 0;
--- server.c
+++ server.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include<arpa/inet.h>
+#include <arpa/inet.h>
 
-#define BUFLEN 512  // Max length of buffer
-#define PORT 1029   // The port on which to listen for incoming data
+#define BUFLEN 1024  // Max length of buffer
+#define PORT 1029    // The port on which to listen for incoming data
 
 struct Registry {
   char name[256];
@@ -14,10 +16,35 @@ struct Registry {
   char ttl[256];
 };
 
+char *trim(char *str) {
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0) return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
+}
+
+int valid_ip(char *ip) {
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
+    return result != 0;
+}
+
 long read_dns_file(FILE *file, struct Registry *registries[]) {
   long count = 0;
-  char line[1024];
+  char line[BUFLEN];
   while (fgets(line, sizeof line, file) != NULL) {
+    // Instance registry
     struct Registry *registry = malloc(sizeof(struct Registry));
     sscanf(line, "%s %s %s %s", registry->name, registry->value, registry->type, registry->ttl);
     registries[count++] = registry;
@@ -54,7 +81,8 @@ int main(int argc, char const *argv[]) {
 
   socklen_t slen;
   int recv_len;
-  char buf[BUFLEN];
+  char buf[BUFLEN]; // incomming
+  char response[BUFLEN]; // outgoing
 
   // Create UDP socket
   int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -85,16 +113,47 @@ int main(int argc, char const *argv[]) {
 
     // Print details of the client/peer and the data received
     printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-    printf("Data: %s\n" , buf);
+
+    // Read message
+    char *message = trim(buf);
+    int reverse_query = valid_ip(message);
+
+    // Find entry
+    struct Registry *registry;
+    for (size_t i = 0; i < size; i++) {
+      registry = registries[i];
+      if (reverse_query && strcmp(message, registry->value) == 0) {
+        break;
+      } else if (!reverse_query && strcmp(message, registry->name) == 0) {
+        break;
+      } else {
+        registry = NULL;
+      }
+    }
+
+    // Create response
+    if (registry != NULL) {
+      strcpy(response, registry->name);
+      strcat(response, " ");
+      strcat(response, registry->value);
+      strcat(response, " ");
+      strcat(response, registry->type);
+      strcat(response, " ");
+      strcat(response, registry->ttl);
+    } else {
+      strcpy(response, "NONE NONE NONE NONE");
+    }
 
     // Now reply the client with the same data
-    if (sendto(sock, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1) {
+    if (sendto(sock, response, BUFLEN, 0, (struct sockaddr*) &si_other, slen) == -1) {
       perror("sendto()");
     }
 
     // Clear the buffer by filling null
     memset(buf, '\0' , BUFLEN);
   }
+
+  // Shutdown socket
   close(sock);
 
   return 0;
