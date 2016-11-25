07e8ab93ac87cde0ac347dccb2f799488a85a71b
Patricio López <lopezjuripatricio@gmail.com>
2016-11-24 21:34:17 -0300

    Setup sockets

--- client.c
+++ client.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define SERVER "127.0.0.1"
+#define BUFLEN 512  // Max length of buffer
+#define PORT 1029   // The port on which to send data
 
 int main(int argc, char const *argv[]) {
-  printf("%s\n", "hello");
+  struct sockaddr_in si_other;
+  socklen_t slen = sizeof(si_other);
+  char buf[BUFLEN];
+  char message[BUFLEN];
+
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sock < 0) {
+    perror("Cannot create socket");
+  }
+
+  // Zero out the structure
+  memset((char *) &si_other, 0, sizeof(si_other));
+
+  si_other.sin_family = AF_INET;
+  si_other.sin_port = htons(PORT);
+
+  if (inet_aton(SERVER, &si_other.sin_addr) == 0) {
+    fprintf(stderr, "inet_aton() failed\n");
+    exit(1);
+  }
+
+  while(1) {
+    printf("Enter message: ");
+    fgets(message, BUFLEN, stdin);
+
+    // Send the message
+    if (sendto(sock, message, strlen(message), 0, (struct sockaddr *) &si_other, slen)==-1) {
+      perror("sendto()");
+    }
+
+    // Clear the buffer by filling null, it might have previously received data
+    memset(buf, '\0' , BUFLEN);
+
+    // Try to receive some data, this is a blocking call
+    if (recvfrom(sock, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1) {
+      perror("recvfrom()");
+    }
+
+    // Show response
+    puts(buf);
+  }
+  close(sock);
+
   return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include<arpa/inet.h>
+
+#define BUFLEN 512  // Max length of buffer
+#define PORT 1029   // The port on which to listen for incoming data
 
 struct Registry {
   char name[256];
@@ -31,6 +36,9 @@ long line_count(FILE *file) {
 }
 
 int main(int argc, char const *argv[]) {
+
+  /* DNS file setup */
+
   FILE *file = fopen("./server.dns", "r");
 
   long size = line_count(file);
@@ -39,9 +47,55 @@ int main(int argc, char const *argv[]) {
 
   fclose(file);
 
-  for (size_t i = 0; i < size; i++) {
-    printf("%s\n", registries[i]->value);
+
+  /* UDP Server setup */
+
+  struct sockaddr_in si_me, si_other;
+
+  socklen_t slen;
+  int recv_len;
+  char buf[BUFLEN];
+
+  // Create UDP socket
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sock < 0) {
+    perror("Cannot create socket");
   }
+
+  // Zero out the structure
+  memset((char *) &si_me, 0, sizeof(si_me));
+
+  si_me.sin_family = AF_INET;
+  si_me.sin_port = htons(PORT);
+  si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  // Bind socket to port
+  if(bind(sock, (struct sockaddr*)&si_me, sizeof(si_me)) == -1) {
+    perror("Cannot bind socket");
+  }
+
+  while(1) {
+    printf("Waiting for data...");
+    fflush(stdout);
+
+    // Try to receive some data, this is a blocking call
+    if ((recv_len = recvfrom(sock, buf, BUFLEN, 0, (struct sockaddr *)&si_other, &slen)) == -1) {
+      perror("recvfrom()");
+    }
+
+    // Print details of the client/peer and the data received
+    printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+    printf("Data: %s\n" , buf);
+
+    // Now reply the client with the same data
+    if (sendto(sock, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1) {
+      perror("sendto()");
+    }
+
+    // Clear the buffer by filling null
+    memset(buf, '\0' , BUFLEN);
+  }
+  close(sock);
 
   return 0;
 }
